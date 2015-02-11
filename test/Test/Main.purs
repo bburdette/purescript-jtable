aa23856af731b4f25c2657638fe722502f1ed021
Ben B <bburdette@gmail.com>
2015-02-11 12:03:02 -0700

    make this stuff compile

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -1,7 +1,36 @@
 module Test where
 
 import Debug.Trace 
+import Data.Argonaut
+-- import Data.Argonaut.Core  
+import qualified Data.Argonaut.Core as C
+import qualified Data.Argonaut.Encode as E
+import qualified Data.Argonaut.Decode as D
+import Data.Identity
+import Data.Either
+import Data.Maybe
+import qualified Data.StrMap as M
+import Data.Tuple
 
+data Foo = Foo
+    { foo :: String
+    , bar :: Number
+    }
+
+instance showFoo :: Show Foo where
+    show (Foo f) = "Foo(" ++ show f.foo ++ ", " ++ show f.bar ++ ")"
+
+instance decodeFoo :: D.DecodeJson Foo where
+    decodeJson json = maybe (Left "not json for foo") Right $ do
+      obj <- C.toObject json
+      foo <- (M.lookup "foo" obj >>= C.toString)
+      bar <- (M.lookup "bar" obj >>= C.toNumber)
+      pure (Foo {foo: foo, bar: bar})
+
+instance encodeFoo :: E.EncodeJson Foo where
+  encodeJson (Foo {foo = f, bar = b}) =  fromObject $ M.fromList [Tuple "foo" $ C.fromString f, Tuple "bar" $ C.fromNumber b]
+
+--    encodeJson :: a -> Json
 main = do 
   trace $ bah
 
